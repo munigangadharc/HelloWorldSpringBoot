a8eae46936165821b93003cebc5bd069b35b6aff
Munigangadhar <115263745+munitechjava@users.noreply.github.com>
2022-10-07 21:31:26 +0530

    firstCommit

---
+++ src/main/java/com/example/demo/HelloWorldSpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldSpringBootApplication {
+	
+	@GetMapping("/hello")
+	public String getMapping()
+	{
+		return "Hello Muni";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+	}
+
+}
